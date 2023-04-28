387545eb32119f53eea95846df8b873409f23340
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-04-28 09:14:49 -0300

    Creating new components

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,14 @@
+import './Banner.css';
+
+function Banner () {
+    // JSX como react trabalha com a parte visual
+    return (
+        <header className='banner'> 
+        <img src="/imagens/banner.png" alt="Banner princiapl do Organo"/>
+        </header>
+
+
+    )
+}
+
+export default Banner
