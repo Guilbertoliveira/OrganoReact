21cac7b56d7c85afe7f9364707978a0b26fe961a
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-05-01 10:36:12 -0300

    Using useState

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,21 +1,22 @@
-import './CampoTexto.css';
+import './CampoTexto.css'; 
 
 // com as {} conseguimos pegar a variavel e inserir o que foi passado pelo props para dentro do nosso html
 const CampoTexto = (props) => {
 
-    let valor = '';
+    // let valor = '';
+
     
     const campoModificado = `${props.placeholder}...`
     const aoDigitado = (evento) => {
-        valor = evento.target.value;
-        console.log(valor);
+        props.aoAlterado(evento.target.value);
     }
+
 
 
     return (
             <div className='Container-campotexto'>
                 <label>{props.label}</label> 
-                <input onChange={aoDigitado} required={props.obrigatorio} placeholder={campoModificado}></input>
+                <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={campoModificado}></input>
             </div>
 
  )
