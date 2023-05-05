c1cb5bf296032f8a880208d456a45e1677b7d345
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-05-05 15:08:09 -0300

    border form

--- src/componentes/Card/index.js
+++ src/componentes/Card/index.js
@@ -4,15 +4,17 @@ const Card = ({nome, imagem, cargo, corPrimaria}) => {
     
     
     return (
-        <div className='card'>
-            <div className='topo' style={{backgroundColor: corPrimaria}}>
-                <img src={imagem} alt={nome} ></img>
-            </div>
-            <div className='rodape'>
-                <h4>{nome} </h4>
-                <h5>{cargo}</h5>
-            </div>
-        </div>
+      
+                <div className='card'>
+                    <div className='topo' style={{backgroundColor: corPrimaria}}>
+                        <img src={imagem} alt={nome} ></img>
+                    </div>
+                    <div className='rodape'>
+                        <h4>{nome} </h4>
+                        <h5>{cargo}</h5>
+                    </div>
+                </div>
+        
     )
 }
 
