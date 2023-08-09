a30563dc0d1fd1f6696e7cdc64d9a2a66b69dc6b
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-08-09 20:41:09 -0300

    refactor: typed component

--- src/componentes/ButtonReact/index.js
+++
@@ -1,9 +0,0 @@
-import './ButtonReact.css';
-
-const ButtonReact = (props) => {
-    return (
-        <button className='Button'>{props.children}</button>  //recebe tudo que está dentro da tag react <ButtonReact> no index.js
-    )
-}
-
-export default ButtonReact;
