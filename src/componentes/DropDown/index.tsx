e019504f58ee8bbb0f771db6fb34d982645e42ae
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-08-11 13:38:21 -0300

    refactor: code typing continued

--- src/componentes/DropDown/index.tsx
+++ src/componentes/DropDown/index.tsx
@@ -1,14 +1,20 @@
 import './DropDown.css';
 
+interface DropDownprops {
+    label: string
+    valor: string
+    aoAlterado: (valor: string) => void
+    itens: string[]
+}
 
-const DropDown = (props) => {
+const DropDown = (props: DropDownprops) => {
 
     return (
         <div className='Lista-suspensa'>
             <label>{props.label}</label>
             <select value={props.valor} onChange={evento => props.aoAlterado(evento.target.value)} >
                 {props.itens.map(item => {
-                    return <option key={item}>{item}</option>
+                    return <option key={item}> {item}</option>
                 })}
             </select>
         </div>
